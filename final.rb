37d4a22670331a311afc40ceec1deaf54ce15f05
kamioka24 <keee2404@gmail.com>
2020-09-08 18:19:04 +0900

    [最終課題]

--- final.rb
+++ final.rb
@@ -1,28 +1,28 @@
 class Student
-	def initialize(name)
-		@name = name
-	end
+  def initialize(name)
+    @name = name
+  end
 
-	def cal_avg(data)
-		sum = 0
-		for score in data do
-			sum += score
-		end
-		avg = sum / data.length
-		return avg
-	end
+  def cal_avg(data)
+    sum = 0
+    for score in data do
+      sum += score
+    end
+    avg = sum / data.length
+    return avg
+  end
 
-	def jedge(avg)
-		result = '' # この行、なくても平気かも？
-		if 60 <= avg
-			result = "passed"
-		else
-			result = "failed"
-		end
-		return result
-	end
+  def jedge(avg)
+    result = '' # この行、なくても平気かも？
+    if 60 <= avg
+      result = "passed"
+    else
+      result = "failed"
+    end
+    return result
+  end
 
-		attr_accessor :name
+  attr_accessor :name
 end
 
 a001 = Student.new("sato")
